f0afb6ab42f976cf87182022ecb0c1cf0fd0aa3d
Mitul Asodariya <70693181+Mitul-778@users.noreply.github.com>
2022-03-05 09:13:42 +0530

    modified

--- Day-2/index.js
+++ Day-2/index.js
@@ -11,18 +11,7 @@ app.get("", function (req, res) {
 app.get("/books", (req, res) => {
  
   const data = require("./book.json")  
-  console.log('data:', data)
   res.send(data);
  
 });
-app.get("/books/:Wingsoffire", (req, res) => {
- 
- console.log(
-  {
-  "title":"Wings of Fire",
-  "author":"APJ Abdul Kalam",
-  "publicationDate":1999
 
-})
- 
-});
