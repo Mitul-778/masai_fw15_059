7d49810a0dfed22514cc71078e418cc85c52c174
Mitul Asodariya <70693181+Mitul-778@users.noreply.github.com>
2022-04-08 21:29:10 +0530

    done

--- Unit-5/Day-1 Webpack/webpack.config.js
+++ Unit-5/Day-1 Webpack/webpack.config.js
@@ -6,5 +6,13 @@ output:{
     filename:"build.js",
 },
 mode:"development",
-
+module: {
+    rules: [
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 }
