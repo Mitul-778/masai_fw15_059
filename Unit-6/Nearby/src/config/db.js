29ff8382ba5ecee0d8282c48c4510bf7d969abb4
Mitul Asodariya <70693181+Mitul-778@users.noreply.github.com>
2022-06-06 09:32:18 +0530

    Update db.js

--- Unit-6/Nearby/src/config/db.js
+++ Unit-6/Nearby/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connect = () =>{
-    return mongoose.connect('mongodb+srv://mitul:patel778@cluster0.ecdww.mongodb.net/nearby?retryWrites=true&w=majority')
+    return mongoose.connect('mongodb://127.0.0.1:27017/geospatial')
 }
 
 module.exports = connect;
