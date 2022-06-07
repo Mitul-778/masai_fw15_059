f01003c4dc6dc2782368adfc4590e5d4f5afde30
Mitul Asodariya <70693181+Mitul-778@users.noreply.github.com>
2022-06-07 18:37:55 +0530

    done

--- Unit-6/Mini-Project/Frontend/src/App.jsx
+++ Unit-6/Mini-Project/Frontend/src/App.jsx
@@ -8,13 +8,18 @@ import { useSearchParams } from "react-router-dom";
 function App() {
 let [searchParams, setSearchParams] = useSearchParams();
 const [data,setData] = useState([])
+const [total_page,setTotal_page] = useState(0)
 const [page,setPage] = useState(searchParams.get('page') || 1)
 const [category,setCategory] = useState(searchParams.get('category') || '')
 const [sort,setSort] = useState(searchParams.get('sort') || '')
 const [color,setColor] = useState(searchParams.get('color') || '')
+const arr = new Array(total_page).fill(0);
 
 useEffect(()=>{
-  axios.get(`http://localhost:5000/product?color=${color}&category=${category}&sort=${sort}&page=${page}`).then((res)=>setData(res.data.product))
+  axios.get(`http://localhost:5000/product?color=${color}&category=${category}&sort=${sort}&page=${page}`).then((res)=>{
+    setData(res.data.product)
+    setTotal_page(res.data.total_page)
+  }).catch((err)=>console.log(err))
   setSearchParams({page,category,sort,color})
 },[page,category,sort,color])
 
@@ -22,18 +27,19 @@ useEffect(()=>{
   return (
     <>
     <div id='filter'>
-      <select name="category" onChange={(e)=>setCategory(e.target.value)} >
+      <select name="category" onChange={(e)=>{setCategory(e.target.value);setPage(1)}} >
         <option value="">Category</option>
         <option value="sedan">Sedan</option>
         <option value="hatch_back">Hatch Back</option>
         <option value="suv">SUV</option>
       </select>
-      <select name="sort"  onChange={(e)=>setSort(e.target.value)}>
+      <select name="sort"  onChange={(e)=>{setSort(e.target.value);setPage(1)}}>
         <option value="">Sort by</option>
         <option value="sort_asc">Asc</option>
         <option value="sort_dsc">Desc</option>
       </select>
-      <select name="color" onChange={(e)=>setColor(e.target.value)}>
+      <select name="color" onChange={(e)=>{setColor(e.target.value);setPage(1)}}>
+        <option value="">Color</option>
         <option value="white">White</option>
         <option value="black">Black</option>
         <option value="gray">Gray</option>
@@ -48,11 +54,9 @@ useEffect(()=>{
     </div>
     <div id="btns">
       <button onClick={()=>setPage(page-1)}>PREV</button>
-      <button onClick={()=>setPage(1)}>1</button>
-      <button onClick={()=>setPage(2)}>2</button>
-      <button onClick={()=>setPage(3)}>3</button>
-      <button onClick={()=>setPage(4)}>4</button>
-      <button onClick={()=>setPage(5)}>5</button>
+      { arr.map((e,i)=>(
+        <button key={i} onClick={()=>setPage(i+1)}>{i+1}</button>
+      )) }
       <button onClick={()=>setPage(page+1)}>NEXT</button>
     </div>
     </>
